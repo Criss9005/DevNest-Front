75f07adaa25664eefadcf9e41d45280af23f2661
Milena Bolaños <mpbolanos0@gmail.com>
2024-05-17 01:18:12 -0500

    The DailyCaloriesForm module was updated

--- src/components/DailyCaloriesForm/DailyCaloriesForm.js
+++ src/components/DailyCaloriesForm/DailyCaloriesForm.js
@@ -4,7 +4,7 @@ import Button from '../Button/Button';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
-const schema = yup.object().shape({
+const validationSchema = yup.object().shape({
   height: yup.number()
     .min(100, 'Height must be greater than or equal to 100')
     .max(220, 'Height must be less than or equal to 220')
@@ -23,176 +23,167 @@ const schema = yup.object().shape({
     .required('Desired weight is required'),
 });
 
-function DailyCaloriesForm({ onFormSubmit, height, age, currentWeight, desiredWeight, bloodType = '1' }) {
+function DailyCaloriesForm({ onFormSubmit }) {
   return (
     <Formik
-    initialValues={{
-      height: '',
-      age: '',
-      currentWeight: '',
-      desiredWeight: '',
-      bloodType: '1' // Valor por defecto
-    }}
-    validationSchema={schema}
-    onSubmit={onFormSubmit}
-    enableReinitialize
+      initialValues={{
+        height: '',
+        age: '',
+        currentWeight: '',
+        desiredWeight: '',
+        bloodType: '1' // Valor por defecto
+      }}
+      validationSchema={validationSchema}
+      onSubmit={onFormSubmit}
     >
       {formik => {
-        const { handleSubmit, isValid, dirty, errors, touched, values } = formik;
+        const { handleSubmit, isValid, dirty, errors, touched } = formik;
         return (
           <Form className={styles['calculate__form']} onSubmit={handleSubmit}>
-              <div className={styles['calculate__field-wrapper']}>
-                <Field
-                  type="height"
-                  name="height"
-                  placeholder=" "
-            className={
-              errors.height && touched.height
-                ? `${styles['calculate__field']} ${styles['input-error']}`
-                : styles['calculate__field']
-            }
-            onFocus={e => e.target.placeholder = ''}
-            onChange={e => e.target.placeholder = ''}
-          />
-          <label htmlFor="height" className={styles['floating-label']}>
-            Height *
-          </label>
-                <ErrorMessage
-                  name="height"
-                  component="div"
-                  className={styles['subtitle-error']}
-                />
-              </div>
-              <div className={styles['calculate__field-wrapper']}>
-                <Field
-                  type="age"
-                  name="age"
-                  placeholder=" "
-            className={
-              errors.age && touched.age
-                ? `${styles['calculate__field']} ${styles['input-error']}`
-                : styles['calculate__field']
-            }
-            onFocus={e => e.target.placeholder = ''}
-            onChange={e => e.target.placeholder = ''}
-          />
-          <label htmlFor="age" className={styles['floating-label']}>
-            Age *
-          </label>
-                <ErrorMessage
-                  name="age"
-                  component="div"
-                  className={styles['subtitle-error']}
-                />
-              </div>
-              <div className={styles['calculate__field-wrapper']}>
-                <Field
-                  type="currentWeight"
-                  name="currentWeight"
-                  placeholder=""
-                  className={
-                    errors.CurrentW && touched.CurrentW
-                      ? styles['input-error']
-                      : styles['calculate__field']
-                  }
-                  onFocus={e => e.target.placeholder = ''}
-            onChange={e => e.target.placeholder = ''}
-          />
-          <label htmlFor="currentWeight" className={styles['floating-label']}>
-          Current weight *
-          </label>
-                
-                <ErrorMessage
-                  name="currentWeight"
-                  component="div"
-                  className={styles['subtitle-error']}
-                />
-              </div>
-              <div className={styles['calculate__field-wrapper']}>
-                <Field
-                  type="desiredWeight"
-                  name="desiredWeight"
-                  placeholder=""
-                  className={
-                    errors.DesiredW && touched.DesiredW
-                      ? styles['input-error']
-                      : styles['calculate__field']
-                  }
-                  onFocus={e => e.target.placeholder = ''}
-            onChange={e => e.target.placeholder = ''}
-          />
-          <label htmlFor="desiredWeight" className={styles['floating-label']}>
-          Desired weight *
-          </label>
-                
-                <ErrorMessage
-                  name="desiredWeight"
-                  component="div"
-                  className={styles['subtitle-error']}
-                />
-              </div>
-
-              <div className={styles['calculate__radio-wrapper']}>
-                <div id={styles['blood-group']}>Blood type *</div>
-                <div
-                  role="group"
-                  aria-labelledby="blood-group"
-                  className={styles['calculate__radio']}
-                >
-                  <label>
-                    1
-                    <Field
-                      type="radio"
-                      name="bloodType"
-                      value="1"
-                      className={styles['calculate__radio-item']}
-                      /*checked*/
-                    />
-                    <span className={styles['checkmark']}></span>
-                  </label>
-                  <label>
-                    2
-                    <Field
-                      type="radio"
-                      name="bloodType"
-                      value="2"
-                      className={styles['calculate__radio-item']}
-                    />
-                    <span className={styles['checkmark']}></span>
-                  </label>
-                  <label>
-                    3
-                    <Field
-                      type="radio"
-                      name="bloodType"
-                      value="3"
-                      className={styles['calculate__radio-item']}
-                    />
-                    <span className={styles['checkmark']}></span>
-                  </label>
-                  <label>
-                    4
-                    <Field
-                      type="radio"
-                      name="bloodType"
-                      value="4"
-                      className={styles['calculate__radio-item']}
-                    />
-                    <span className={styles['checkmark']}></span>
-                  </label>
-                </div>
-              </div>
-
-              <Button
-                id={'button-form'}
-                type="submit"
-                disabled={!(dirty && isValid && values.bloodType)}
+            <div className={styles['calculate__field-wrapper']}>
+              <Field
+                type="number"
+                name="height"
+                placeholder=" "
                 className={
-                  !(dirty && isValid && values.bloodType) ? 'disabled-btn' : ''
+                  errors.height && touched.height
+                    ? `${styles['calculate__field']} ${styles['input-error']}`
+                    : styles['calculate__field']
                 }
-                title={'Start losing weight'}
               />
-            </Form>
+              <label htmlFor="height" className={styles['floating-label']}>
+                Height *
+              </label>
+              <ErrorMessage
+                name="height"
+                component="div"
+                className={styles['subtitle-error']}
+              />
+            </div>
+
+            <div className={styles['calculate__field-wrapper']}>
+              <Field
+                type="number"
+                name="age"
+                placeholder=" "
+                className={
+                  errors.age && touched.age
+                    ? `${styles['calculate__field']} ${styles['input-error']}`
+                    : styles['calculate__field']
+                }
+              />
+              <label htmlFor="age" className={styles['floating-label']}>
+                Age *
+              </label>
+              <ErrorMessage
+                name="age"
+                component="div"
+                className={styles['subtitle-error']}
+              />
+            </div>
+
+            <div className={styles['calculate__field-wrapper']}>
+              <Field
+                type="number"
+                name="currentWeight"
+                placeholder=" "
+                className={
+                  errors.currentWeight && touched.currentWeight
+                    ? `${styles['calculate__field']} ${styles['input-error']}`
+                    : styles['calculate__field']
+                }
+              />
+              <label htmlFor="currentWeight" className={styles['floating-label']}>
+                Current weight *
+              </label>
+              <ErrorMessage
+                name="currentWeight"
+                component="div"
+                className={styles['subtitle-error']}
+              />
+            </div>
+
+            <div className={styles['calculate__field-wrapper']}>
+              <Field
+                type="number"
+                name="desiredWeight"
+                placeholder=" "
+                className={
+                  errors.desiredWeight && touched.desiredWeight
+                    ? `${styles['calculate__field']} ${styles['input-error']}`
+                    : styles['calculate__field']
+                }
+              />
+              <label htmlFor="desiredWeight" className={styles['floating-label']}>
+                Desired weight *
+              </label>
+              <ErrorMessage
+                name="desiredWeight"
+                component="div"
+                className={styles['subtitle-error']}
+              />
+            </div>
+
+            <div className={styles['calculate__radio-wrapper']}>
+              <div id={styles['blood-group']}>Blood type *</div>
+              <div
+                role="group"
+                aria-labelledby="blood-group"
+                className={styles['calculate__radio']}
+              >
+                <label>
+                  1
+                  <Field
+                    type="radio"
+                    name="bloodType"
+                    value="1"
+                    className={styles['calculate__radio-item']}
+                  />
+                  <span className={styles['checkmark']}></span>
+                </label>
+                <label>
+                  2
+                  <Field
+                    type="radio"
+                    name="bloodType"
+                    value="2"
+                    className={styles['calculate__radio-item']}
+                  />
+                  <span className={styles['checkmark']}></span>
+                </label>
+                <label>
+                  3
+                  <Field
+                    type="radio"
+                    name="bloodType"
+                    value="3"
+                    className={styles['calculate__radio-item']}
+                  />
+                  <span className={styles['checkmark']}></span>
+                </label>
+                <label>
+                  4
+                  <Field
+                    type="radio"
+                    name="bloodType"
+                    value="4"
+                    className={styles['calculate__radio-item']}
+                  />
+                  <span className={styles['checkmark']}></span>
+                </label>
+              </div>
+            </div>
+
+            <Button
+              id={'button-form'}
+              type="submit"
+              disabled={!(dirty && isValid && formik.values.bloodType)}
+              className={
+                !(dirty && isValid && formik.values.bloodType) ? 'disabled-btn' : ''
+              }
+              title={'Start losing weight'}
+            />
+          </Form>
         );
       }}
     </Formik>
