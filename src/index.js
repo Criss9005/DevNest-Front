280e595b2306578252ad0cf9b7dd0867cb2dba76
Criss9005 <cdjpn5@gmail.com>
2024-05-17 21:28:14 -0500

    organizando pages

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <App />
+    
   </React.StrictMode>
 );
