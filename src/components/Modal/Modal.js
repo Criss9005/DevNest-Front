a0ddaee77f6f85f3bcd6238b99e3525ea3237b90
Michael Caceres <michaeling10@hotmail.com>
2024-05-21 21:52:20 -0500

    Modal Esc and click outside

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import css from './modal.module.css';
 
 const Modal = ({ isOpen, closeModal, children }) => {
+  useEffect(() => {
+    const handleEsc = event => {
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleEsc);
+    } else {
+      document.removeEventListener('keydown', handleEsc);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, [isOpen, closeModal]);
+
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className={`${css.modal} ${isOpen ? css.isopen : ''}`}>
-      <div className={css.modcontainer}>
+    <div
+      className={`${css.modal} ${isOpen ? css.isopen : ''}`}
+      onClick={closeModal}
+    >
+      <div className={css.modcontainer} onClick={e => e.stopPropagation()}>
         <button onClick={closeModal} className={css.modalclose}>
           X
         </button>
