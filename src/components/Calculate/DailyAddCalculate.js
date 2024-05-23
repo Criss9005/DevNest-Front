ced7194dcec3a89024e2d8bee6515669a5d952d9
Ediison32 <edisonalvarez32@gmail.com>
2024-05-23 01:46:24 -0500

    modificaciones

--- src/components/Calculate/DailyAddCalculate.js
+++ src/components/Calculate/DailyAddCalculate.js
@@ -2,18 +2,25 @@
 import DailyCaloriesForm from "../DailyCaloriesForm/DailyCaloriesForm";
 import Calculator from "../Calculate/Calculate";
 import style from '../Calculate/calculato.module.css';
-
+import Header from "components/Header/headerComponent";
 
 export default function DailyAddCalculator(){
 
     return(
-            <div className={style['main__container']}>
-                <div className={style['main__content']}>
-                    <DailyCaloriesForm/>
-                </div>
+            <div>
+                <div className={style['main__container']}>
+                        <Header/>
+                
+                    <div className={style['main__content']}>
+                        <DailyCaloriesForm/>
+                    </div>               
+                </div>   
+             
                 <div className={style['calculate__container']}>
-                    <Calculator/>
-                </div>
-            </div>        
+                        <Calculator/>
+                </div> 
+            </div>
+            
+           
     );
 }
--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Calculate from 'components/Calculate/DailyAddCalculate'
-import MenuLogged from 'components/Menu/MenuLogged'
+/* import MenuLogged from 'components/Menu/MenuLogged' */
+
 
 export default function Calculator() {
   return (
     <div>
-      <MenuLogged/>
+     
       <Calculate/>
     </div>
   )
