c51889092d644749e3e4f4e7e74c20bd5056fb07
Criss9005 <cdjpn5@gmail.com>
2024-05-23 22:10:12 -0500

    ok

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -1,6 +1,5 @@
 import DailyCaloriesForm from "components/DailyCaloriesForm/DailyCaloriesForm";
 import Calculator from "components/Calculate/Calculate";
-import MenuLogged from 'components/Menu/MenuLogged';
 import style from 'components/Calculate/calculato.module.css';
 import BackgroundCalculate from "components/BackgroundCalculate/BackgroundCalculate";
 
@@ -8,7 +7,7 @@ export default function DailyAddCalculator() {
   return (
     <div className={style['main__container']}>
                 <div className={style['main__content']}>
-                    <MenuLogged/>
+                    
                     <DailyCaloriesForm/>
                 </div>
                 <div className={style['calculate__container']}>
