08bdb9e51d8bdb5e22ed71db1a57f5f49c4903da
Criss9005 <cdjpn5@gmail.com>
2024-05-23 22:01:21 -0500

    logout check

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Calculate from 'components/Calculate/DailyAddCalculate'
 import Header from 'components/Header/headerComponent'
-//import MenuLogged from 'components/Menu/MenuLogged'
+
 
 export default function Calculator() {
   return (
     <div>
-      {/* <MenuLogged/> */}
       <Header/>
       <Calculate/>
     </div>
