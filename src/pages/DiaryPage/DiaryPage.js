135228ffc96e50d3b197fdcacc22f27525857df5
Milena Bolaños <mpbolanos0@gmail.com>
2024-05-24 01:05:24 -0500

    Updade new styles of DiaryPage and MainPage

--- src/pages/DiaryPage/DiaryPage.js
+++ src/pages/DiaryPage/DiaryPage.js
@@ -1,7 +1,7 @@
 
 import Calculator from "components/Calculate/Calculate";
 import style from 'components/Calculate/calculato.module.css';
-
+import Header from 'components/Header/headerComponent' 
 import Diary from 'components/diary/Diary';
 import BackgroundCalculate from "components/BackgroundCalculate/BackgroundCalculate";
 
@@ -10,7 +10,7 @@ export default function DiaryAddCalculator(){
     return(
             <div className={style['main__container']}>
                 <div className={style['main__content']}>
-                
+                    <Header/> 
                     <Diary/>
                 </div>
                 <div className={style['calculate__container']}>
