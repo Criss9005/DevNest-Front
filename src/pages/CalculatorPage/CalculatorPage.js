8484dca895d60f635e5e66f69efbb912cab16eb8
Michael Caceres <michaeling10@hotmail.com>
2024-05-27 20:03:40 -0500

    Local storage update and css main page

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -1,17 +1,14 @@
-import DailyCaloriesForm from "components/DailyCaloriesForm/DailyCaloriesForm";
-import Calculator from "components/Calculate/Calculate";
+import DailyCaloriesForm from 'components/DailyCaloriesForm/DailyCaloriesForm';
+import Calculator from 'components/Calculate/Calculate';
 import style from 'components/Calculate/calculato.module.css';
 // import BackgroundCalculate from "components/BackgroundCalculate/BackgroundCalculate";
-import Header from "components/Header/Header";
-import MenuLogged from "components/MenuLogged/MenuLogged";
-import css from './CalculatorPage.module.css'
+import Header from 'components/Header/Header';
+import MenuLogged from 'components/MenuLogged/MenuLogged';
+import css from './CalculatorPage.module.css';
 
 //import { useEffect, useState } from "react";
 
-
-
 export default function DailyAddCalculator() {
-  
   /* const [data, setData] = useState
 
   useEffect(() => { 
@@ -20,20 +17,20 @@ export default function DailyAddCalculator() {
       setData(JSON.parse(newData))
     }
   },[]) */
-  
+
   return (
-    <div className={style['main__container'] }>
+    <div className={style['main__container']}>
       <div className={style['main__content']}>
-        <div className={css.headerContainer }>
-        <Header />
-        <MenuLogged />
+        <div className={css.headerContainer}>
+          <Header />
+          <MenuLogged />
         </div>
         <DailyCaloriesForm />
-        </div>
-            <div className={style['calculate__container']}>
-                    <Calculator />
-                    {/* <BackgroundCalculate/> */}
-                </div>
-            </div> 
+      </div>
+      <div className={style['calculate__container']}>
+        <Calculator />
+        {/* <BackgroundCalculate/> */}
+      </div>
+    </div>
   );
 }
